9372c4399d4abca23d6c1bcecfff6806e890a4de
ricardoalvim <ricardo.ricardo815@gmail.com>
2016-11-16 12:10:40 -0200

    Release final do desenho colab

---
+++ Desenho Colaborativo - Servidor/Trabalho 3 - Servidor/Servidor.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Trabalho_3___Servidor
+{
+    partial class Servidor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.BT_Ligar = new System.Windows.Forms.Button();
+            this.CB_Padrao = new System.Windows.Forms.CheckBox();
+            this.TB_Porta = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TB_IP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TB_Log = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Servidor - Definições";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BT_Ligar
+            // 
+            this.BT_Ligar.Location = new System.Drawing.Point(191, 120);
+            this.BT_Ligar.Name = "BT_Ligar";
+            this.BT_Ligar.Size = new System.Drawing.Size(75, 23);
+            this.BT_Ligar.TabIndex = 14;
+            this.BT_Ligar.Text = "Ligar";
+            this.BT_Ligar.UseVisualStyleBackColor = true;
+            this.BT_Ligar.Click += new System.EventHandler(this.BT_Ligar_Click);
+            // 
+            // CB_Padrao
+            // 
+            this.CB_Padrao.AutoSize = true;
+            this.CB_Padrao.Location = new System.Drawing.Point(15, 120);
+            this.CB_Padrao.Name = "CB_Padrao";
+            this.CB_Padrao.Size = new System.Drawing.Size(102, 17);
+            this.CB_Padrao.TabIndex = 13;
+            this.CB_Padrao.Text = "IP/Porta padrão";
+            this.CB_Padrao.UseVisualStyleBackColor = true;
+            this.CB_Padrao.CheckedChanged += new System.EventHandler(this.CB_Padrao_CheckedChanged);
+            // 
+            // TB_Porta
+            // 
+            this.TB_Porta.Location = new System.Drawing.Point(15, 93);
+            this.TB_Porta.Name = "TB_Porta";
+            this.TB_Porta.Size = new System.Drawing.Size(252, 20);
+            this.TB_Porta.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Porta:";
+            // 
+            // TB_IP
+            // 
+            this.TB_IP.Location = new System.Drawing.Point(15, 53);
+            this.TB_IP.Name = "TB_IP";
+            this.TB_IP.Size = new System.Drawing.Size(252, 20);
+            this.TB_IP.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "IP:";
+            // 
+            // TB_Log
+            // 
+            this.TB_Log.Enabled = false;
+            this.TB_Log.Location = new System.Drawing.Point(12, 149);
+            this.TB_Log.Multiline = true;
+            this.TB_Log.Name = "TB_Log";
+            this.TB_Log.Size = new System.Drawing.Size(259, 132);
+            this.TB_Log.TabIndex = 15;
+            // 
+            // Servidor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 292);
+            this.Controls.Add(this.TB_Log);
+            this.Controls.Add(this.BT_Ligar);
+            this.Controls.Add(this.CB_Padrao);
+            this.Controls.Add(this.TB_Porta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TB_IP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Servidor";
+            this.Text = "Servidor";
+            this.Load += new System.EventHandler(this.Servidor_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BT_Ligar;
+        private System.Windows.Forms.CheckBox CB_Padrao;
+        private System.Windows.Forms.TextBox TB_Porta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TB_IP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TB_Log;
+    }
+}
+
---
+++ Desenho Colaborativo - Servidor/Trabalho 3 - Servidor/Servidor.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Pacote;
+
+namespace Trabalho_3___Servidor
+{
+    public partial class Servidor : Form
+    {
+        IPAddress ip;
+        int porta;
+        IPEndPoint host;
+        Socket servidor;
+        Thread Touvinte;
+        List<Socket> clientes;
+        Bitmap desenho;
+
+
+        public Servidor()
+        {
+            InitializeComponent();
+        }
+
+        private void Servidor_Load(object sender, EventArgs e)
+        {
+            clientes = new List<Socket>();
+        }
+
+        private void BT_Ligar_Click(object sender, EventArgs e)
+        {
+            if (!CB_Padrao.Checked)
+            {
+                if (TB_IP.Text.Equals("") || TB_Porta.Text.Equals(""))
+                {
+                    MessageBox.Show("Preencha o IP/Porta", "Erro na entrada de dados",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+            }
+
+            TB_Log.Text += "[Ligando...]" + Environment.NewLine;
+            ligar();
+            TB_IP.Enabled = false;
+            CB_Padrao.Enabled = false;
+            TB_Porta.Enabled = false;
+            BT_Ligar.Enabled = false;
+            BT_Ligar.Text = "Ligado";
+        }
+
+        private void ligar()
+        {
+            if (CB_Padrao.Checked)
+            {
+                ip = IPAddress.Parse("127.0.0.1");
+                porta = int.Parse("2000");
+            }
+            else
+            {
+                ip = IPAddress.Parse(TB_IP.Text);
+                porta = int.Parse(TB_Porta.Text);
+            }
+
+            host = new IPEndPoint(ip, porta);
+            servidor = new Socket(AddressFamily.InterNetwork,
+                  SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                servidor.Bind(host);
+                Touvinte = new Thread(new ThreadStart(ouvinte));
+                Touvinte.Start();
+                TB_Log.Text += "[Ligado]" + Environment.NewLine;
+            }
+            catch (Exception ex)
+            {
+                TB_Log.Text += "[Erro]" + Environment.NewLine;
+                MessageBox.Show("Não foi possível iniciar o servidor neste IP/Porta\n\nErro: " + ex.ToString(), "Erro na conexão",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+        }
+
+        private void ouvinte()
+        {
+            servidor.Listen(50);
+            while (true)
+            {
+                Socket temp = servidor.Accept();
+                clientes.Add(temp);
+                Thread t = new Thread(new ParameterizedThreadStart(trata_Cliente));
+                t.Start(temp);
+            }
+        }
+
+        private void trata_Cliente(object obj)
+        {
+            Socket cliente = (Socket)obj;
+            while (true)
+            {
+                byte[] dados = new byte[999999];
+                try
+                {
+                    cliente.Receive(dados, SocketFlags.None);
+                }
+                catch { return; }
+                Pacote.Pacote receivePack = new Pacote.Pacote();
+                receivePack = (Pacote.Pacote)ByteArrayToObject(dados);
+
+                switch (receivePack.Id)
+                {
+                    case Identificador.Login:
+                        AppendTextBox("Cliente conectado de: " + cliente.LocalEndPoint + Environment.NewLine);
+                        Console.WriteLine("\n\nAlguém logou!\n");
+                        break;
+                    case Identificador.Logout:
+                        foreach (Socket c in clientes)
+                        {
+                            if (c == cliente)
+                            {
+                                clientes.Remove(c);
+                                break;
+                            }
+                        }
+                        Console.WriteLine("\n\nLogout!\n");
+                        break;
+                    case Identificador.Info:
+                        Console.WriteLine("\nInfo!\n");
+                        if (desenho != null)
+                        {
+                            Graphics g = Graphics.FromImage(desenho);
+                            g.DrawImage(receivePack.Imagem, new Point(0, 0));
+                        }
+                        else
+                        {
+                            desenho = receivePack.Imagem;
+                        }
+
+                        Pacote.Pacote sendPack = new Pacote.Pacote();
+                        sendPack.Id = Identificador.Info;
+                        sendPack.Imagem = desenho;
+                        dados = new byte[999999];
+                        dados = ObjectToByteArray(sendPack);
+                        foreach (Socket c in clientes)
+                        {
+                            c.Send(dados, SocketFlags.None);
+                            Console.WriteLine("enviou!" + Environment.NewLine);
+                        }
+                        if (desenho != null)
+                        {
+                            desenho.Save("batman.png");
+                        }
+                        else
+                        {
+                            Console.WriteLine("qede");
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void CB_Padrao_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CB_Padrao.Checked)
+            {
+                TB_IP.Enabled = false;
+                TB_Porta.Enabled = false;
+            }
+            else
+            {
+                TB_IP.Enabled = true;
+                TB_Porta.Enabled = true;
+            }
+        }
+
+        public void AppendTextBox(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox), new object[] { value });
+                return;
+            }
+            TB_Log.Text += value;
+        }
+
+        #region Conversor
+        // Convert an object to a byte array
+        private byte[] ObjectToByteArray(Object obj)
+        {
+            if (obj == null)
+                return null;
+
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, obj);
+
+            return ms.ToArray();
+        }
+
+        // Convert a byte array to an Object
+        private Object ByteArrayToObject(byte[] arrBytes)
+        {
+            MemoryStream memStream = new MemoryStream();
+            BinaryFormatter binForm = new BinaryFormatter();
+            memStream.Write(arrBytes, 0, arrBytes.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            Object obj = (Object)binForm.Deserialize(memStream);
+            return obj;
+        }
+        #endregion
+    }
+}
